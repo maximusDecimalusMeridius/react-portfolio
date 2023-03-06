d4e06d0833c06e242eb86302a21c4a319459a285
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-03-06 14:04:45 -0800

    Setup conditional rendering and linked components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import React, {useState} from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Project from "./components/Project/Project";
 import './App.css';
 
 function App() {
+  
+  let [page, setPage] = useState("about-me");
+
+  function renderPage(){
+    if(page === "about-me"){
+      return(
+        <div>About Me</div>
+      )
+    } else if (page === "projects") {
+      return(
+        <div>Projects</div>
+      )
+    }
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Project />
+      <Header setPage={setPage} />
+      {renderPage()}
       <Footer />
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Navigation from "../Navigation/Navigation";
 import "./style.css";
 
-function Header(){
+function Header(props){
     return(
         <header>
-            <div>Oh Hi!</div>
-            <Navigation />
+            <div class="header-title">Andy Gaudy</div>
+            <Navigation setPage={props.setPage}/>
         </header>
     );
 };
