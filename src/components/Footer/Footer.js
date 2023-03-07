b7ecb79c7e5a76a0d575f9eda07555e2e90dd7fa
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-03-06 23:07:30 -0800

    Added links and logos to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./Footer.css";
+import fblogo from "../../assets/fb-logo.png";
+import ghlogo from "../../assets/github-logo.png";
+import lilogo from "../../assets/linkedin-logo.png";
 
 function Footer(){
     return(
         <footer>
-            Trademark
+            <a href="https://www.facebook.com/andy.ayooo" target="_blank" rel="noreferrer"><img className="logo-pic" src={fblogo} alt="facebook logo"/></a>
+            <a href="https://www.github.com/maximusDecimalusMeridius" target="_blank" rel="noreferrer"><img className="logo-pic" src={ghlogo} alt="github logo"/></a>
+            <a href="https://www.linkedin.com/in/the-recruiter-that-is-technical" target="_blank" rel="noreferrer"><img className="logo-pic" src={lilogo} alt="linkedin logo"/></a>
         </footer>
     )
 };
