4d29ce209bec0dd3c9349039fa8fc0d31bf65da6
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-03-07 09:57:51 -0800

    Added trademark to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,9 +7,12 @@ import lilogo from "../../assets/linkedin-logo.png";
 function Footer(){
     return(
         <footer>
-            <a href="https://www.facebook.com/andy.ayooo" target="_blank" rel="noreferrer"><img className="logo-pic" src={fblogo} alt="facebook logo"/></a>
-            <a href="https://www.github.com/maximusDecimalusMeridius" target="_blank" rel="noreferrer"><img className="logo-pic" src={ghlogo} alt="github logo"/></a>
-            <a href="https://www.linkedin.com/in/the-recruiter-that-is-technical" target="_blank" rel="noreferrer"><img className="logo-pic" src={lilogo} alt="linkedin logo"/></a>
+            <div className="footerTop">
+                <a href="https://www.facebook.com/andy.ayooo" target="_blank" rel="noreferrer"><img className="logo-pic" src={fblogo} alt="facebook logo"/></a>
+                <a href="https://www.github.com/maximusDecimalusMeridius" target="_blank" rel="noreferrer"><img className="logo-pic" src={ghlogo} alt="github logo"/></a>
+                <a href="https://www.linkedin.com/in/the-recruiter-that-is-technical" target="_blank" rel="noreferrer"><img className="logo-pic" src={lilogo} alt="linkedin logo"/></a>
+            </div>
+            <p className="trademark">© Andy Gaudy 2023</p>
         </footer>
     )
 };
