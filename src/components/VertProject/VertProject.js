94e4ababe18a45dea7a498c476078ff9074fcf56
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-03-08 16:16:38 -0800

    Added project data file and iterate to generate vertMenu

--- src/components/VertProject/VertProject.js
+++ src/components/VertProject/VertProject.js
@@ -1,15 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import "./VertProject.css";
-import roomeasePic from "../../assets/thumbs/roomease.png";
-import superheroPic from "../../assets/thumbs/superhero-game.png";
-import quizPic from "../../assets/thumbs/quiz-game.png";
-import codePic from "../../assets/thumbs/code-snippets.png";
-import weatherPic from "../../assets/thumbs/weather-tracker.png";
-import blogPic from "../../assets/thumbs/blog-app.png";
+import projectData from "../../projectData.js";
 
 function VertProject(){
     
     const [lastClicked, setLastClicked] = useState("card-1");
+    const [cardCount, setCardCount] = useState(0);
 
     function handleClick (event) {
         if(event.target.id.includes("card") && !event.target.classList.contains("grow")){
@@ -39,96 +35,41 @@ function VertProject(){
         }
 }
 
+const projectCards = projectData.map((project, index) => 
+<div className="vert-card" id={`card-${index + 1}`} onClick={handleClick}>
+            <div className="title" id="title">{`${index + 1}`}</div>
+            <div className="vertContent" id="vertContent">
+                <div className="vertFacts">
+                    <ul className="vertFactsList">
+                        <li className="fact"><span className="firstWord">Description:</span> {project.description}</li>
+                        <li className="fact"><span className="firstWord">Tech:</span> {project.tech}</li>
+                        <li className="fact"><span className="firstWord">Learnings:</span> {project.learnings}</li>
+                        <li><a href={project.liveURL} target="_blank" rel="noreferrer">Live Site</a> | <a href={project.githubURL} target="_blank" rel="noreferrer">GitHub</a></li>
+                    </ul>
+                </div>
+                <img className="menuPic" src={project.image} alt={project.title}/>
+            </div>
+        </div>
+    )
+
+    //useEffect() on load
+    // add function to apply grow to vert-card on the first card on load
+    // add function to apply hideNode to title on the first card on load
+    // add function to apply showNode,fadeInNode to vertContent on the first card on load
+
+    useEffect(() => { 
+        document.getElementById("card-1").classList.add("grow");
+        document.getElementById("title").classList.add("hideNode");
+        document.getElementById("vertContent").classList.add("showNode", "fadeInNode");
+     }, []);
+
     return(
         <div className="vertProjectContainer">
             <div className="vert-menu">
-                <div className="vert-card grow" id="card-1" onClick={handleClick}>
-                    <div className="title hideNode">1</div>
-                    <div className="vertContent showNode fadeInNode">
-                        <div className="vertFacts">
-                            <ul className="vertFactsList">
-                                <li className="fact"><span className="firstWord">Description:</span> Full-stack application to manage roommates, events, tasks, and "You-Owe-Mes"</li>
-                                <li className="fact"><span className="firstWord">Tech:</span> HTML/CSS/JS/Express/Node.js/MySQL</li>
-                                <li className="fact"><span className="firstWord">Learnings:</span> ERD development and implementation, signup/login auth, npm packages, project management</li>
-                                <li><a href="https://roomease.herokuapp.com" target="_blank" rel="noreferrer">Live Site</a> | <a href="https://www.github.com/maximusDecimalusMeridius/roomease" target="_blank" rel="noreferrer">GitHub</a></li>
-                            </ul>
-                        </div>
-                        <img className="menuPic" src={roomeasePic} alt="project thumbnail"/>
-                    </div>
-                </div>
-                <div className="vert-card" id="card-2" onClick={handleClick}>
-                    <div className="title">2</div>
-                    <div className="vertContent">
-                    <div className="vertFacts">
-                            <ul className="vertFactsList">
-                                <li className="fact"><span className="firstWord">Description:</span> Superhero multi-track card game with randomization and infant AI</li>
-                                <li className="fact"><span className="firstWord">Tech:</span> HTML/CSS/JS/Materialize CSS & jQuery</li>
-                                <li className="fact"><span className="firstWord">Learnings:</span> API fetch requests, local storage, game design, event listeners & animations</li>
-                                <li><a href="https://maximusdecimalusmeridius.github.io/git-good/arena/" target="_blank" rel="noreferrer">Live Site</a> | <a href="https://www.github.com/maximusDecimalusMeridius/git-good" target="_blank" rel="noreferrer">GitHub</a></li>
-                            </ul>
-                        </div>
-                        <img className="menuPic" src={superheroPic} alt="project thumbnail"/>
-                    </div>
-                </div>
-                <div className="vert-card" id="card-3" onClick={handleClick}>
-                    <div className="title">3</div>
-                    <div className="vertContent">
-                    <div className="vertFacts">
-                            <ul className="vertFactsList">
-                                <li className="fact"><span className="firstWord">Description:</span> Code Quiz Game</li>
-                                <li className="fact"><span className="firstWord">Tech:</span> HTML/CSS/JS</li>
-                                <li className="fact"><span className="firstWord">Learnings:</span> localStorage, JS event listeners, managing data in arrays</li>
-                                <li><a href="https://maximusdecimalusmeridius.github.io/my-quiz-game" target="_blank" rel="noreferrer">Live Site</a> | <a href="https://www.github.com/maximusDecimalusMeridius/my-quiz-game" target="_blank" rel="noreferrer">GitHub</a></li>
-                            </ul>
-                        </div>
-                        <img className="menuPic" src={quizPic} alt="project thumbnail"/>
-                    </div>
-                </div>
-                <div className="vert-card" id="card-4" onClick={handleClick}>
-                    <div className="title">4</div>
-                    <div className="vertContent">
-                    <div className="vertFacts">
-                            <ul className="vertFactsList">
-                                <li className="fact"><span className="firstWord">Description:</span> A single-page vanilla JS application displaying code snippets</li>
-                                <li className="fact"><span className="firstWord">Tech:</span> HTML/CSS/JS</li>
-                                <li className="fact"><span className="firstWord">Learnings:</span> HTML layout, JS event listeners, CSS styling</li>
-                                <li><a href="https://maximusdecimalusmeridius.github.io/snippet-cheatsheet" target="_blank" rel="noreferrer">Live Site</a> | <a href="https://www.github.com/maximusDecimalusMeridius/snippet-cheatsheet" target="_blank" rel="noreferrer">GitHub</a></li>
-                            </ul>
-                        </div>
-                        <img className="menuPic" src={codePic} alt="project thumbnail"/>
-                    </div>
-                </div>
-                <div className="vert-card" id="card-5" onClick={handleClick}>
-                    <div className="title">5</div>
-                    <div className="vertContent">
-                    <div className="vertFacts">
-                            <ul className="vertFactsList">
-                                <li className="fact"><span className="firstWord">Description:</span> A single-page weather tracking app that uses API fetch requests</li>
-                                <li className="fact"><span className="firstWord">Tech:</span> HTML/CSS/JS/APIs</li>
-                                <li className="fact"><span className="firstWord">Learnings:</span> Mobile-first design, API fetch requests, page layout & formatting</li>
-                                <li><a href="https://maximusdecimalusmeridius.github.io/weather-tracker" target="_blank" rel="noreferrer">Live Site</a> | <a href="https://www.github.com/maximusDecimalusMeridius/weather-tracker" target="_blank" rel="noreferrer">GitHub</a></li>
-                            </ul>
-                        </div>
-                        <img className="menuPic" src={weatherPic} alt="project thumbnail"/>
-                    </div>
-                </div>
-                <div className="vert-card" id="card-6" onClick={handleClick}>
-                    <div className="title">6</div>
-                    <div className="vertContent">
-                    <div className="vertFacts">
-                            <ul className="vertFactsList">
-                                <li className="fact"><span className="firstWord">Description:</span> Full-stack blog application that allows users to create blog posts and comment on others' posts</li>
-                                <li className="fact"><span className="firstWord">Tech:</span> HTML/CSS/JS/Express/Node.js/MySQL</li>
-                                <li className="fact"><span className="firstWord">Learnings:</span> ERD design and joins, MVC model & handlebars, sessions, sign-up and login auth</li>
-                                <li><a href="https://blogzzz.herokuapp.com" target="_blank" rel="noreferrer">Live Site</a> | <a href="https://www.github.com/maximusDecimalusMeridius/blog-app" target="_blank" rel="noreferrer">GitHub</a></li>
-                            </ul>
-                        </div>
-                        <img className="menuPic" src={blogPic} alt="project thumbnail"/>
-                    </div>
-                </div>
+                {projectCards}
             </div>
         </div>
-    );
+    )
 };
 
 export default VertProject;
--- src/projectData.js
+++ src/projectData.js
@@ -1,52 +1,64 @@
 /* eslint-disable import/no-anonymous-default-export */
+import roomeaseLogo from "./assets/thumbs/roomease.png";
+import superheroLogo from "./assets/thumbs/superhero-game.png";
+import quizgameLogo from "./assets/thumbs/quiz-game.png";
+import cheatsheetLogo from "./assets/thumbs/code-snippets.png";
+import weatherLogo from "./assets/thumbs/weather-tracker.png";
+import blogLogo from "./assets/thumbs/blog-app.png";
 
 export default[
-
-{
-    title: "Roomease",
-    description: "Full-stack application to manage roommates, events, tasks, and 'You-owe-Mes'",
-    tech: "HTMl/CSS/JS/Express/Node.js/MySQL",
-    learnings: "ERD development and implementation, signup/login auth, npm packages, project management",
-    liveURL: "https://roomease.herokuapp.com",
-    githubURL: "https://www.github.com/maximusDecimalusMeridius/roomease"
-},
-{
-    title: "Roomease",
-    description: "Full-stack application to manage roommates, events, tasks, and 'You-owe-Mes'",
-    tech: "HTMl/CSS/JS/Express/Node.js/MySQL",
-    learnings: "ERD development and implementation, signup/login auth, npm packages, project management",
-    liveURL: "https://roomease.herokuapp.com",
-    githubURL: "https://www.github.com/maximusDecimalusMeridius/roomease"
-},
-{
-    title: "Roomease",
-    description: "Full-stack application to manage roommates, events, tasks, and 'You-owe-Mes'",
-    tech: "HTMl/CSS/JS/Express/Node.js/MySQL",
-    learnings: "ERD development and implementation, signup/login auth, npm packages, project management",
-    liveURL: "https://roomease.herokuapp.com",
-    githubURL: "https://www.github.com/maximusDecimalusMeridius/roomease"
-},
-{
-    title: "Roomease",
-    description: "Full-stack application to manage roommates, events, tasks, and 'You-owe-Mes'",
-    tech: "HTMl/CSS/JS/Express/Node.js/MySQL",
-    learnings: "ERD development and implementation, signup/login auth, npm packages, project management",
-    liveURL: "https://roomease.herokuapp.com",
-    githubURL: "https://www.github.com/maximusDecimalusMeridius/roomease"
-},
-{
-    title: "Roomease",
-    description: "Full-stack application to manage roommates, events, tasks, and 'You-owe-Mes'",
-    tech: "HTMl/CSS/JS/Express/Node.js/MySQL",
-    learnings: "ERD development and implementation, signup/login auth, npm packages, project management",
-    liveURL: "https://roomease.herokuapp.com",
-    githubURL: "https://www.github.com/maximusDecimalusMeridius/roomease"
-},
-{
-    title: "Roomease",
-    description: "Full-stack application to manage roommates, events, tasks, and 'You-owe-Mes'",
-    tech: "HTMl/CSS/JS/Express/Node.js/MySQL",
-    learnings: "ERD development and implementation, signup/login auth, npm packages, project management",
-    liveURL: "https://roomease.herokuapp.com",
-    githubURL: "https://www.github.com/maximusDecimalusMeridius/roomease"
-}
+    {
+        title: "Roomease",
+        image: roomeaseLogo,
+        description: "Full-stack application to manage roommates, events, tasks, and 'You-Owe-Mes'",
+        tech: "HTML/CSS/JS/Express/Node.js/MySQL",
+        learnings: "ERD development and implementation, signup/login auth, npm packages, project management",
+        liveURL: "https://roomease.herokuapp.com",
+        githubURL: "https://www.github.com/maximusDecimalusMeridius/roomease"
+    },
+    {
+        title: "Superhero Card Game",
+        image: superheroLogo,
+        description: "Superhero multi-track card game with randomization and infant AI",
+        tech: "HTML/CSS/JS/Materialize CSS & jQuery",
+        learnings: "API fetch requests, local storage, game design, event listeners & animations",
+        liveURL: "https://maximusdecimalusmeridius.github.io/git-good/arena/",
+        githubURL: "https://www.github.com/maximusDecimalusMeridius/git-good"
+    },
+    {
+        title: "Code Quiz Game",
+        image: quizgameLogo,
+        description: "Quiz game with random question selection, text coloring, and local storage",
+        tech: "HTML/CSS/JS",
+        learnings: "localStorage, JS event listeners, managing data in arrays",
+        liveURL: "https://maximusdecimalusmeridius.github.io/my-quiz-game",
+        githubURL: "https://www.github.com/maximusDecimalusMeridius/my-quiz-game"
+    },
+    {
+        title: "Snippet Cheatsheet",
+        image: cheatsheetLogo,
+        description: "A single-page vanilla JS application displaying code snippets",
+        tech: "HTML/CSS/JS",
+        learnings: "ERD development and implementation, signup/login auth, npm packages, project management",
+        liveURL: "https://roomease.herokuapp.com",
+        githubURL: "https://www.github.com/maximusDecimalusMeridius/roomease"
+    },
+    {
+        title: "Weather Tracker",
+        image: weatherLogo,
+        description: "A single-page weather tracking app that uses API fetch requests",
+        tech: "HTML/CSS/JS/APIs",
+        learnings: "Mobile-first design, API fetch requests, page layout & formatting",
+        liveURL: "https://maximusdecimalusmeridius.github.io/weather-tracker",
+        githubURL: "https://www.github.com/maximusDecimalusMeridius/weather-tracker"
+    },
+    {
+        title: "Blog App",
+        image: blogLogo,
+        description: "Full-stack blog application that allows users to create blog posts and comment on others' posts",
+        tech: "HTML/CSS/JS/Express/Node.js/MySQL",
+        learnings: "ERD design and joins, MVC model & handlebars, sessions, sign-up and login auth",
+        liveURL: "https://blogzzz.herokuapp.com",
+        githubURL: "https://www.github.com/maximusDecimalusMeridius/blog-app"
+    },
+]
